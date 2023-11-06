5b7cfd547c8b57e356644694ba8a406f6e41e3d6
trung <you@example.com>
2023-11-06 13:03:57 +0700

    get rid of prinwriter and add jsp redirection

--- src/main/java/qotd/LoginServlet.java
+++ src/main/java/qotd/LoginServlet.java
@@ -1,11 +1,12 @@
 package qotd;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import javax.servlet.ServletException;
 
 import javax.servlet.annotation.WebServlet;
 
@@ -18,15 +19,8 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Trung Đẹp Zai!!</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<h1>Trung Đẹp Zai!!</h1>");
-		out.println("</body>");
-		out.println("</html>");
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+												throws IOException, ServletExeception {
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
 	}
 }
