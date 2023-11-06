745e41ccbf40364d597773f2040d7353a38b7ca4
trung <you@example.com>
2023-11-06 13:07:26 +0700

    modification for running on server

--- src/main/java/qotd/LoginServlet.java
+++ src/main/java/qotd/LoginServlet.java
@@ -20,7 +20,7 @@ public class LoginServlet extends HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
-												throws IOException, ServletExeception {
+												throws IOException, ServletException {
 		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
 	}
 }
