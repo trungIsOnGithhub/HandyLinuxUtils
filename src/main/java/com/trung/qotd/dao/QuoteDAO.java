661c016ebd60a5ec7c0a93cd42f1968d941938e9
trungIsOnGithhub <trung150502@gmail.com>
2023-11-07 11:59:43 +0700

    add logic to dao

--- src/main/java/com/trung/qotd/dao/QuoteDAO.java
+++ src/main/java/com/trung/qotd/dao/QuoteDAO.java
@@ -2,8 +2,27 @@ package com.trung.qotd.dao;
 
 import com.trung.qotd.entity.Quote;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuoteDAO {
+	private static List<Quote> quotes;
+	
+	public QuoteDAO() {
+		if (quotes == null)
+			quotes = new ArrayList<>();
+	}
+
 	public void saveOne(Quote quote) {
-		System.out.println("Saved Quote!!!");
+		quotes.add(quote);
+	}
+	
+	public Quote getLatestOne(Quote quote) {
+		int lastIndex = quotes.size() - 1;
+
+		if (lastIndex < 0)
+			return null;
+
+		return quotes.get(lastIndex);
 	}
 }
