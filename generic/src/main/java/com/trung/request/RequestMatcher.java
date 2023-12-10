1bca8b660ff10301c3f7a39eac4769606c43dc5c
trungvuive <trung150502@gmail.com>
2023-12-10 16:00:06 +0700

    add matcher

---
+++ generic/src/main/java/com/trung/request/RequestMatcher.java
@@ -0,0 +1,28 @@
+package com.trung.request;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class RequestMatcher {
+    //valid request line in the http protocol
+    public static final Pattern VALID_REQUEST_LINE = Pattern.compile("[A-Z]{3,} /.{0,} HTTP/1.[0-1]\\r\\n"); 
+     //valid header in the http protocol
+    public static final Pattern VALID_HEADER = Pattern.compile(".{1,}: .{1,}\r\n");
+    //valid input for hipotetical /simpleForm.html file
+    public static final Pattern SIMPLEFORM_PAYLOAD_FORMAT = Pattern.compile("[{]StudentName:.{0,},StudentID:.{0,}[}]");
+
+    public static boolean isRequestLineValid(String requestLine) {
+        Matcher requestLineMatcher = VALID_REQUEST_LINE.matcher(requestLine);
+        return requestLineMatcher.matches();
+    }
+
+    public static boolean isHeaderValid(String header) {
+        Matcher headerMatcher = VALID_HEADER.matcher(header);
+        return headerMatcher.matches();
+    }
+
+    public static boolean isSimpleFormPayloadValid(String payload) {
+    	Matcher payloadMatcher = SIMPLEFORM_PAYLOAD_FORMAT.matcher(payload);
+        return payloadMatcher.matches();
+    }
+}
