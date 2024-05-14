26e834605e27b288818ece6f6110a8aad5dbb113
Trung Nguyen <trung150502@gmail.com>
2024-05-14 12:32:13 +0000

    delete old code repo

--- src/main/java/com/mv/trung/carent/actions/consts/Cookies.java
+++
@@ -1,9 +0,0 @@
-package com.mv.schelokov.carent.actions.consts;
-
-/**
- *
- * @author Maxim Chshelokov <schelokov.mv@gmail.com>
- */
-public class Cookies {
-    public static final String LOCALE="locale";
-}
