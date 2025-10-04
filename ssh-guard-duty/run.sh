5dc5de7db40c12afe5b6deda5a649667d2293451
trungvuive <trung150502@gmail.com>
2025-10-05 06:11:14 +0700

    add script for handling unrecognized success login

--- ssh-guard-duty/run.sh
+++ ssh-guard-duty/run.sh
@@ -11,21 +11,37 @@ INCOMING_WEBHOOK_URL=
 DATE_STR=$(date +"%Y-%m-%d_%H:%M:%S")
 
 log_alert() {
-    DATE_STR="$(date '+%Y-%m-%d__%H:%M:%S') - $1"
-    echo "$DATE_STR" | tee -a "$ALERT_LOG"
-}
+        DATE_STR="$(date '+%Y-%m-%d__%H:%M:%S') - $1"
+        echo "$DATE_STR" | tee -a "$ALERT_LOG"
+    }
 
-send_alert() {
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"$"}' $INCOMING_WEBHOOK_URL
+    send_alert() {
+        curl -X POST -H 'Content-type: application/json' --data '{"text":"$"}' $INCOMING_WEBHOOK_URL
 
-if [ ! -f "$KNOWN_IPS_LOCATION" ]; then
-    touch "$KNOWN_IPS_LOCATION"
-fi
-
-FAILED_ATTEMPTS=$(grep "$HOUR_AGO" "$AUTH_LOG" | grep -i "failed password\|invalid user" | awk '{print $11}' | sort | uniq -c | sort -nr)
-
-echo "$FAILED_ATTEMPTS" | while read count ip; do
-    if [ "$count" -gt "$FAILED_THRESHOLD" ] && [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        log_alert "Exccedded failed login: $count attempts from IP $ip"
+    if [ ! -f "$KNOWN_IPS_LOCATION" ]; then
+        touch "$KNOWN_IPS_LOCATION"
     fi
-done
+
+    FAILED_ATTEMPTS=$(grep "$HOUR_AGO" "$AUTH_LOG" | grep -i "failed password\|invalid user" | awk '{print $11}' | sort | uniq -c | sort -nr)
+
+    echo "$FAILED_ATTEMPTS" | while read count ip; do
+        if [ "$count" -gt "$FAILED_THRESHOLD" ] && [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+            log_alert "Exccedded failed login: $count attempts from IP $ip"
+        fi
+    done
+
+
+    # Handling for unrecognized sucessful login within 1 day
+    SUCCESSFUL_LOGINS=$(grep "$DAY_AGO" "$AUTH_LOG" | grep -i "accepted password\|accepted publickey" | awk '{print $11}' | sort | uniq)
+
+    while read ip; do
+        if ! grep -q "^$ip$" "$KNOWN_IPS_FILE"; then
+            log_alert "New successful SSH login from unknown IP: $ip"
+            echo "$ip" >> "$KNOWN_IPS_FILE"
+        fi
+    done <<< "$SUCCESSFUL_LOGINS"
+
+    if [ -f "$KNOWN_IPS_FILE" ]; then
+        awk -v date="$DAY_AGO" '$0 > date {print}' "$KNOWN_IPS_FILE" > "${KNOWN_IPS_FILE}.tmp" && mv "${KNOWN_IPS_FILE}.tmp" "$KNOWN_IPS_FILE"
+    fi
+}
